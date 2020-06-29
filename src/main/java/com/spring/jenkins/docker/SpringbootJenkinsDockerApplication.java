7b2e362f34219928edb487a1bfb67bee9dd48d1f
sumithmelbin <sumithmathew123@gmail.com>
2020-06-29 15:50:41 -0500

    added second API(/hello)

--- src/main/java/com/spring/jenkins/docker/SpringbootJenkinsDockerApplication.java
+++ src/main/java/com/spring/jenkins/docker/SpringbootJenkinsDockerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @SpringBootApplication
 public class SpringbootJenkinsDockerApplication implements CommandLineRunner {
@@ -18,9 +19,14 @@ public class SpringbootJenkinsDockerApplication implements CommandLineRunner {
 		System.out.println("Running on Docker Container...");
 
 	}
+
 	@GetMapping("/")
 	public String display() {
 		return "This is running from Docker Container Using JENKINS CI ";
 	}
 
+	@GetMapping("/hello")
+	public String message() {
+		return "Hi Sumith I am from docker container... ";
+	}
 }
