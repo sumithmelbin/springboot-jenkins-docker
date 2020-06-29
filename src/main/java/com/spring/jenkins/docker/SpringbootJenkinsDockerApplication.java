8461a217bad402c28156720fe31b1a08781e68b8
sumithmelbin <sumithmathew123@gmail.com>
2020-06-29 14:51:29 -0500

    Implemented CommandLine Runner

--- src/main/java/com/spring/jenkins/docker/SpringbootJenkinsDockerApplication.java
+++ src/main/java/com/spring/jenkins/docker/SpringbootJenkinsDockerApplication.java
@@ -1,13 +1,20 @@
 package com.spring.jenkins.docker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJenkinsDockerApplication {
+public class SpringbootJenkinsDockerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDockerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello Sumith");
+
 	}
 
 }
