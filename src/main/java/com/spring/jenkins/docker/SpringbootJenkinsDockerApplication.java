5aeb26196f5432a3a540a4c38df231b608f37229
sumithmelbin <sumithmathew123@gmail.com>
2020-06-29 14:35:53 -0500

    first commit

---
+++ src/main/java/com/spring/jenkins/docker/SpringbootJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jenkins/docker/SpringbootJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
